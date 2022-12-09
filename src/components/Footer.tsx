db21637964d1f2656f0c1d94349069582c28f525
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2022-12-09 21:07:15 +0330

    Initial commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+interface IProps {}
+
+const Footer: React.FC<IProps> = (props) => {
+  return (
+    <section className="footer__wrapper">This is Footer (Part Three)</section>
+  );
+};
+
+export default Footer;
